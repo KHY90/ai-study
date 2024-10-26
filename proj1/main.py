65ef8dcb92bc053aa0563bfcb1f0eb0abfa295f3
KHY90 <power4206@gmail.com>
2024-10-26 16:03:38 +0900

    first day

---
+++ proj1/main.py
@@ -0,0 +1,15 @@
+# # 가상환경 목록 조회
+# conda env list
+
+# # 가상환경 만들기
+# conda create -n proj1 python=3.12
+# conda remove -n proj1 가상환경 삭제
+
+# # 가상환경 진입하기
+# conda activate proj1
+
+# # numpy 설치
+# pip install numpy
+
+import numpy
+
